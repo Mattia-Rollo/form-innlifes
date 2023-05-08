8274856a1bbc9954c48547d968c4b6f20121caeb
mattia.rollo <ste.tia@hotmail.it>
2023-05-08 22:40:02 +0200

    style filepond

---
+++ filepond.js
@@ -0,0 +1,16 @@
+FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.setOptions({
+  allowDrop: true,
+  allowReplace: true,
+  imagePreviewMinHeight: 50,
+  imagePreviewMaxHeight: 150,
+  instantUpload: false,
+  dropOnPage: true,
+  dropValidation: true,
+  ignoredFiles: [".ds_store", "thumbs.db", "desktop.ini"],
+  allowFileTypeValidation: true,
+  acceptedFileTypes: ["image/jpg"],
+});
+
+FilePond.parse(document.body);
+// console.log(FilePond.getOptions());
