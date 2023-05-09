672bc7b001654081904ed1b8c75387f544a9798d
mattia.rollo <ste.tia@hotmail.it>
2023-05-09 10:19:30 +0200

    style filepond

--- filepond.js
+++ filepond.js
@@ -1,19 +1,34 @@
-FilePond.registerPlugin(FilePondPluginImagePreview);
+FilePond.registerPlugin(
+  FilePondPluginFileValidateSize,
+
+  FilePondPluginImagePreview
+);
 FilePond.setOptions({
-  allowDrop: true,
-  allowReplace: true,
+  // allowDrop: true,
+  // allowReplace: true,
   imagePreviewMinHeight: 50,
   imagePreviewMaxHeight: 150,
   instantUpload: false,
-  dropOnPage: true,
-  dropValidation: true,
+  // dropOnPage: true,
+  // dropValidation: true,
   ignoredFiles: [".ds_store", "thumbs.db", "desktop.ini"],
-  allowFileTypeValidation: true,
-  acceptedFileTypes: ["image/jpg"],
+  // allowFileTypeValidation: true,
+  // acceptedFileTypes: ["image/jpg"],
+  maxFileSize: "1MB",
+  labelMaxFileSize: "Grandezza Massima consentita {filesize}",
+  // onprocessfileerror: function (error, file) {
+  //   alert("Errore durante il caricamento del file: " + error);
+  // },
+  labelFileProcessingCompleteError:
+    "Si è verificato un errore durante il caricamento del file.",
+  labelFileProcessingAbortedError: "Il caricamento del file è stato annullato.",
+  labelFileProcessingError:
+    "Si è verificato un errore durante il caricamento del file.",
+  labelMaxFileSizeExceeded: "Attenzione Il file è troppo grande!",
+  plugins: [FilePondPluginFileValidateSize],
   labelIdle:
     'Trascina e rilascia qui i tuoi file oppure <span class="filepond--label-action">Sfoglia</span>',
 });
 
-FilePond.registerPlugin(FilePondPluginImageTransform);
 FilePond.parse(document.body);
 // console.log(FilePond.getOptions());
